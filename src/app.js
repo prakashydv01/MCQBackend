bfc58b3947169d148b759aec103a6cebcc30ff02
prakashydv01 <pk143lovek@gmail.com>
2025-06-14 16:55:23 +0545

    print cors origin

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
+console.log("CORS_ORIGIN", process.env.CORS_ORIGIN)
 
 app.use(express.json( {limit : "16kb"} ))
 app.use(express.urlencoded({ extended: true, limit: "16kb"}))
